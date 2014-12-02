708558bed2c1b5ed18f9925a557fd042948c1f46
龚 俊慧 <258644279@qq.com>
2014-12-03 00:13:13 +0800

    江门->添加新闻普通cell

--- jiangmen/jmrb/JmrbNews/AppDelegate.m
+++ jiangmen/jmrb/JmrbNews/AppDelegate.m
@@ -205,13 +205,13 @@ CG_INLINE  void deleteFile() {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     
     // 进行应用程序一系列属性的初始化设置
     [AppPropertiesInitialize startAppPropertiesInitialize];
     
-    BaseTabBarVC *baseTabBarController = [[BaseTabBarVC alloc] init];
-    baseTabBarController.viewControllers = @[[[UINavigationController alloc] initWithRootViewController:[NewsManagerVC new]]];
+    BaseTabBarVC *baseTabBarController = [[[BaseTabBarVC alloc] init] autorelease];
+    baseTabBarController.viewControllers = @[[[[UINavigationController alloc] initWithRootViewController:[[NewsManagerVC new] autorelease]] autorelease]];
     
     self.window.rootViewController = baseTabBarController;
     
