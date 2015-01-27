7f8687d76a077c3b18e10931021da6a847016a82
swift <swift@swiftdeiMac.local>
2015-01-27 14:34:24 +0800

    APP模板->注册&登录BC新增方法

--- APP项目模板/最新模板(能兼容64位)/MuBan/MuBan/Model/Register&Login/LoginBC.h
+++ APP项目模板/最新模板(能兼容64位)/MuBan/MuBan/Model/Register&Login/LoginBC.h
@@ -9,17 +9,27 @@
 #import <Foundation/Foundation.h>
 #import "NetRequestManager.h"
 
+static NSString * const kLoginSuccessNotificationKey = @"kLoginSuccessNotificationKey";
+
 typedef void (^successHandle) (id successInfoObj);
 typedef void (^failedHandle)  (NSError *error);
 
-
 @interface LoginBC : NSObject<NetRequestDelegate>
 
+// 账号密码登录
 - (void)loginWithUserName:(NSString *)userName
                  password:(NSString *)password
                 autoLogin:(BOOL)autoLogin
                   showHUD:(BOOL)show
             successHandle:(successHandle)success
              failedHandle:(failedHandle)failed;
+
+// 动态验证码登录
+- (void)dynamicLoginWithUserName:(NSString *)userName
+                verificationCode:(NSString *)code
+                       autoLogin:(BOOL)autoLogin
+                         showHUD:(BOOL)show
+                   successHandle:(successHandle)success
+                    failedHandle:(failedHandle)failed;
 
 @end
